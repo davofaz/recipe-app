151e47fb08a5bb64fdda0ed695d3a851361ee160
davofaz <dfazaldin@gmail.com>
2023-06-26 14:56:26 +0100

    refactor: test code updated and deleted one

--- src/tests/App.test.tsx
+++ src/tests/App.test.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, screen} from '@testing-library/react';
 import App from '../App';
 
 
 test('renders the App component', () => {
     // Arrange
-    const { getByText } = render(<App />);
+    render(<App />);
 
     // Act
-    const headingElement = getByText(/Recipe Finder/i);
+    const headingElement = screen.getByText(/Recipe Finder/i);
 
     // Assert
     expect(headingElement).toBeInTheDocument();
