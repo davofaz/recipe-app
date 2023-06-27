af2fafaf1089d15bd29106178c84e0a01b576fd5
davofaz <dfazaldin@gmail.com>
2023-06-27 11:53:07 +0100

    feat: added select by cuisine

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,54 @@ import React, { ChangeEvent, useEffect, useState } from 'react';
 import './App.css';
 
 interface Recipe {
-    value: string;
+    //value: string;
+    readyInMinutes: number;
+    sourceUrl: string;
+    image: string;
+    servings: number;
+    id: number;
+    title: string;
+    searchResults: null;
+    //cuisine: string;
+    //setCuisine: string;
 }
+
+const cuisines: string[] = [
+    'African',
+    'Asian',
+    'American',
+    'British',
+    'Cajun',
+    'Caribbean',
+    'Chinese',
+    'Eastern European',
+    'European',
+    'French',
+    'German',
+    'Greek',
+    'Indian',
+    'Irish',
+    'Italian',
+    'Japanese',
+    'Jewish',
+    'Korean',
+    'Latin American',
+    'Mediterranean',
+    'Mexican',
+    'Middle Eastern',
+    'Nordic',
+    'Southern',
+    'Spanish',
+    'Thai',
+    'Vietnamese'
+];
+
 
 const App: React.FC = () => {
 
     const [search, setSearch] = useState<string>('');
     const [searchResults, setSearchResults] = useState<Recipe[]>([]);
+    const [cuisine, setCuisine] = useState<string>('');
 
     const handleSearch = (e:ChangeEvent<HTMLInputElement>) => {
         setSearch(e.target.value)
@@ -20,11 +61,11 @@ const App: React.FC = () => {
 
     try {
       // Perform API request using the search query
-        const response = await fetch(`https://api.spoonacular.com/recipes/search?apiKey=4afde5867b95408da83b5616319634e9&search=${search}`);
+        const response = await fetch(`https://api.spoonacular.com/recipes/complexSearch?apiKey=4afde5867b95408da83b5616319634e9&query=${search}&cuisine=${cuisine}`);
         const data = await response.json();
 
       // Update the search results state
-        setSearchResults(data.hits);
+        setSearchResults(data.results);
 
     } catch (error) {
       console.error('Error occurred during API request:', error);
@@ -35,6 +76,9 @@ const App: React.FC = () => {
         console.log(searchResults);
     }, [searchResults]);
 
+    const handleCuisineChange = (e: ChangeEvent<HTMLSelectElement>) => {
+        setCuisine(e.target.value);
+    }
 
   return (
     <div className="App">
@@ -48,12 +92,25 @@ const App: React.FC = () => {
                     onChange={handleSearch} 
                     placeholder="Search for a Recipe" 
                   />
+                  <select id="cuisine" value={cuisines} onChange={handleCuisineChange}>
+                  <option value="">-- Select Cuisine --</option>
+                  {cuisines.map((cuisine) => (
+                      <option key={cuisine} value={cuisine}>
+                          {cuisine}
+                      </option>
+                  ))}
+                  </select>
                 <button type="submit">Submit</button>
               </form>
           </header>
 
           <div className="App-main">
               <p>Your results for: {search}</p>
+              <ul>
+                  {searchResults.map((recipe) => (
+                      <li key={recipe.id}>{recipe.title}</li>
+                  ))}
+              </ul>
               
           </div>
     </div>
