146bf85bd4e28d541f9dd75a807adbf4798760b0
davofaz <dfazaldin@gmail.com>
2023-06-29 19:30:42 +0100

    refactor: added craco to try to fix tailwind issues but didnt work so removed it

--- src/App.tsx
+++ src/App.tsx
@@ -106,7 +106,8 @@ const App: React.FC = () => {
                       </option>
                   ))}
                   </select>
-                <button type="submit">Submit</button>
+                  <button type="submit" className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">Submit</button>
+                  <button type="submit" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Clear</button>
               </form>
           </header>
 
