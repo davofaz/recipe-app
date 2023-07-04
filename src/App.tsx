655a130fa8bcc481ef0daa2d1568b4b79b7b249b
davofaz <dfazaldin@gmail.com>
2023-07-04 13:53:25 +0100

    feat: smaller fixed size image, added a bookmark button

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, { ChangeEvent, useEffect, useState } from 'react';
-import { MdReadMore, MdOutlineFoodBank, MdOutlineTimer, MdOutlineGroups } from "react-icons/md";
+import { MdReadMore, MdOutlineFoodBank, MdOutlineTimer, MdOutlineGroups, MdBookmarkAdd } from "react-icons/md";
 
 
 
@@ -101,27 +101,36 @@ const App: React.FC = () => {
 
     const RecipeInfo: React.FC<RecipeInfoProps> = ({ recipe }) => {
         return (
-            <li>
+            <li className="mb-5">
                 <ul>
                     <li><h2 className="mb-2">{recipe.title}</h2></li>
-                    <li><a href={recipe.sourceUrl} target="_blank" rel="noreferrer"><img className="mb-2" src={`https://spoonacular.com/recipeImages/${recipe.image}`} alt={recipe.title} /></a></li>
-                    <li className="flex flex-row">
-                        <a href={recipe.sourceUrl} target="_blank" rel="noreferrer">
-                            <button className="my-btn-rose mb-6"><MdReadMore size={24} /></button>
-                        </a>
-                        <div className="m-2 flex flex-row">
+                    <li className="flex flex-row items-end">
+                        <div className="w-[270px]">
+                            <a href={recipe.sourceUrl} target="_blank" rel="noreferrer">
+                                <img className="mb-2 object-cover h-60 w-80" src={`https://spoonacular.com/recipeImages/${recipe.image}`} alt={recipe.title} />
+                            </a>
+                        </div>
+                        <div className="flex flex-col p-2 items-center">                                                     
                             {recipe.readyInMinutes && (
-                                <div className="flex flex-row">
+                                <div className="flex flex-col items-center">
                                     <MdOutlineTimer size={24} />
-                                    <p className="ml-2">Ready in {recipe.readyInMinutes} mins</p>
+                                    <p className="ml-2 text-center pt-1 pb-2">Ready in {recipe.readyInMinutes} mins</p>
                                 </div>
                             )}
                             {recipe.servings && (
-                                <div className="flex flex-row">
+                                <div className="flex flex-col p-2 items-center"> 
                                     <MdOutlineGroups className="ml-3" size={24} />
-                                    <p className="ml-2">{recipe.servings} servings</p>
+                                    <p className="ml-2 text-center pt-1 pb-2">{recipe.servings} servings</p>
                                 </div>
                             )}
+                            <div className="flex flex-row place-content-evenly">
+                                <a href={recipe.sourceUrl} target="_blank" rel="noreferrer">
+                                    <button className="my-btn-rose m-2 mb-0 items-center"><MdBookmarkAdd size={24} /></button>
+                                </a>
+                                <a href={recipe.sourceUrl} target="_blank" rel="noreferrer">
+                                    <button className="my-btn-rose m-2 mb-0 items-center"><MdReadMore size={24} /></button>
+                                </a>
+                            </div> 
                         </div>
                     </li>
                 </ul>
