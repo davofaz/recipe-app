2b6157f5255566624e15279e5700cd70a1b2ef7d
davofaz <dfazaldin@gmail.com>
2023-07-07 19:58:57 +0100

    feat: added Hashrouter and made some adjustments to routes, links

--- src/index.tsx
+++ src/index.tsx
@@ -1,25 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { createBrowserRouter,  RouterProvider } from 'react-router-dom';
+//import { createBrowserRouter,  RouterProvider } from 'react-router-dom';
+import { createHashRouter, RouterProvider } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import ErrorPage from './pages/ErrorPage';
 import BookmarkPage from './pages/BookmarkPage'
 
-const router = createBrowserRouter([
+const router = createHashRouter([
   {
     path: "/",
     element: <App />,
-    errorElement: <ErrorPage />,
-     children: [
-      {
-        path: "bookmarks",
-             element: <BookmarkPage />
-      },
-    ],
+    errorElement: <ErrorPage />,    
     },
     {
-        path: "bookmarks2",
+        path: "bookmarks",
         element: <BookmarkPage />,
         errorElement: <ErrorPage />,
     },
